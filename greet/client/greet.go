2d8688cba579b9ac4ebe5ddbe14b5ef30348fd0a
Shreeyash-Naik <shreeyashnaikofficial@gmail.com>
2023-03-09 21:41:50 +0530

    feat: init, unary greet service

---
+++ greet/client/greet.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	pb "github.com/shreeyashnaik/grpc-go/greet/proto"
+)
+
+func doGreet(c pb.GreetServiceClient) {
+	log.Println("doGreet was invoked")
+
+	res, err := c.Greet(context.Background(), &pb.GreetRequest{
+		FirstName: "Shreeyash",
+	})
+
+	if err != nil {
+		log.Fatalf("Could not greet: %v\n", err)
+	}
+
+	log.Printf("Greeting: %s\n", res.Result)
+}
